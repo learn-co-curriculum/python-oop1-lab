6422f1b513d06893b1a4fd742477de3557d3be06
aidadieudonne <aidaharing10@gmail.com>
2025-04-29 19:11:50 -0600

    made book and coffee classes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        if isinstance(page_count, int):
+            self.page_count = page_count
+        else: print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
-        
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        if size in ["Small", "Medium", "Large"]:
+            self.size = size
+        else:
+            print("size must be Small, Medium, or Large")
+        self.price = price
+
+        def tip(self):
+            print("This coffee is great, here's a tip!")
+            self.price += 1
+        
