ffd40d0b2c51a83af8211bb507dea509cac1037c
aidadieudonne <aidaharing10@gmail.com>
2025-04-29 21:12:30 -0600

    Think I completed all required things

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 class Coffee:
+    #Initialize Coffe with required size and price 
     def __init__(self, size, price):
         if size in ["Small", "Medium", "Large"]:
             self.size = size
@@ -8,6 +9,7 @@ class Coffee:
             print("size must be Small, Medium, or Large")
         self.price = price
 
+    #Tip method will add 1 to the price
         def tip(self):
             print("This coffee is great, here's a tip!")
             self.price += 1
