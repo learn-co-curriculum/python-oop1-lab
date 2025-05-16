e08b50b4b113bdb4b4af14a59e92b275f35829b0
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-15 23:27:40 -0400

    Add Book and Coffee classes with properties and methods

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def turn_page(self):
+            print("Flipping the page...wow, you read fast!")
+
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
         
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count
+        
+    @page_count.setter
+    def page_count(self, value):
+        """page_count must be an integer"""
+
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+    pass
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Coffee:
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    def tip(self):
+        self.price += 1
+        print("This coffee is great, here’s a tip!")    
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+        
+    @size.setter
+    def size(self, value):
+        """size must be Small, Medium, or Large"""
+
+        if value in ("Small", "Medium", "Large"):
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
     pass
