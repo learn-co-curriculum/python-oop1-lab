cdaf6d62bde6d54a3f595a95cf7437b0c7c131bd
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-16 16:38:23 -0400

    Adds comments to code and updates README file to include functionality of application and a screenshot of code.

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
-
+# Define a class named Coffee
 class Coffee:
+    # Constructor method initializing a Coffee instance with size and price attributes
     def __init__(self, size, price):
-        self.size = size
-        self.price = price
+        self.size = size          # Set the size attribute
+        self.price = price        # Set the price attribute
 
+    # Method to add a tip to the price of the coffee and prints a message
     def tip(self):
-        self.price += 1
+        self.price += 1           # Increases the price attribute by 1
         print("This coffee is great, here’s a tip!")    
 
+    # Define property getter for size
     @property
     def size(self):
         """The size property"""
         return self._size
         
+    # define a setter for size property    
     @size.setter
     def size(self, value):
         """size must be Small, Medium, or Large"""
-
+        # Check if value is one of the allowed sizes 
         if value in ("Small", "Medium", "Large"):
-            self._size = value
+            self._size = value     # Set the _size attribute
         else:
+        # If not, print an error message
             print("size must be Small, Medium, or Large")
     pass
